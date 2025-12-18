adcc9a81c451c74a719daba4dd7192827eca3185
dudu <mieum0915@kakao.com>
2025-12-18 16:18:02 +0900

    ㅎ

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,12 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: isProd ? "export" : undefined,
-  basePath: isProd ? "/remin" : undefined,
+  basePath: isProd ? "/REMIN" : undefined,
   images: {
     unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? "/remin" : "",
+    NEXT_PUBLIC_BASE_PATH: isProd ? "/REMIN" : "",
   },
 };
 
